0ada32724089cf991151741b0597c754d771870e
Fineply <egorzaika11@mail.ru>
2022-10-18 21:31:33 +0300

    Prac3

---
+++ src/ru/mirea/task3/Prac3/Massiv3ex/MASSIV.java
@@ -0,0 +1,26 @@
+/*
+package ru.mirea.task3.Prac3.Massiv3ex;
+
+public class MASSIV {
+    public static void main(String[] args) {
+        int[] Mas = new int[4];
+        for (int i = 0; i < Mas.length; i++) {
+            Mas[i] = (int) (Math.random() * 90 + 10);
+            System.out.print(Mas[i] + " ");
+        }
+            for (int i = 0; i < Mas.length; i++) {
+                if (i > 0) {
+                    if (Mas[i - 1] >= Mas[i]) {
+                        System.out.println("Прогрессия не возрастающая");
+                        break;
+
+                    }
+                }
+
+            if(i==Mas.length-1)
+                System.out.println("Последовательность строго возрастающая");
+        }
+    }
+}
+
+ */
---
+++ src/ru/mirea/task3/Prac3/Obolochki/Obolochki.java
@@ -0,0 +1,47 @@
+/*
+package ru.mirea.task3.Prac3.Obolochki;
+public class Obolochki {
+    public static void main(String[] args) {
+        Double double1 = Double.valueOf("4.6");
+        System.out.println(double1);
+    }
+}
+
+package ru.mirea.task3.Prac3.Obolochki;
+import static java.lang.Double.parseDouble;
+
+public class Obolochka {
+    public static void main(String[] args) {
+        String str="30";
+        String d = Double.toString(3.14);
+        System.out.println(d);
+    }
+}
+
+package ru.mirea.task3.Prac3.Obolochki;
+public class Obolochki {
+    public static void main(String[] args) {
+        double a = 36.575;
+        int b = (int) a;
+        byte c = (byte) a;
+        char r = (char) a;
+        short j = (short) a;
+        long k = (long) a;
+        float m = (float) a;
+        boolean z = true;
+        if (z) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+
+        }
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(r);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(m);
+    }
+}
+
+ */
